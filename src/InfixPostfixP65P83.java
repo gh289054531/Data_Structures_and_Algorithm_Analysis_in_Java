a1694970961b3da9e3d209b76ecd4837ce17df4c
gh289054531 <289054531@qq.com>
2013-12-04 16:49:07 +0800

    optimize

--- src/InfixPostfixP65P83.java
+++ src/InfixPostfixP65P83.java
@@ -1,120 +1,116 @@
 import java.util.ArrayList;
 import java.util.Stack;
 
+/**
+ * P67 计算后缀表达式值、中缀表达式转后缀表达式、后缀表达式转为中缀表达式
+ */
 public class InfixPostfixP65P83 {
-	// 计算后缀表达式的值
+	/**
+	 * 计算后缀表达式的值
+	 */
 	public static Double postfix(String[] input) {
-		Stack<Double> stack = new Stack<Double>();
+		Stack<Double> ops = new Stack<Double>();
 		for (int i = 0; i < input.length; i++) {
 			String cur = input[i];
-			switch (cur) {
+			switch (cur) {// 这种写法只能在java7用
 			case "+":
-				Double x1 = stack.pop();
-				Double y1 = stack.pop();
-				stack.push(x1 + y1);
+				Double x1 = ops.pop();
+				Double y1 = ops.pop();
+				ops.push(x1 + y1);
 				break;
 			case "-":
-				Double x2 = stack.pop();
-				Double y2 = stack.pop();
-				stack.push(y2 - x2);
+				Double x2 = ops.pop();
+				Double y2 = ops.pop();
+				ops.push(y2 - x2);
 				break;
 			case "*":
-				Double x3 = stack.pop();
-				Double y3 = stack.pop();
-				stack.push(x3 * y3);
+				Double x3 = ops.pop();
+				Double y3 = ops.pop();
+				ops.push(x3 * y3);
 				break;
 			case "/":
-				Double x4 = stack.pop();
-				Double y4 = stack.pop();
-				stack.push(y4 / x4);
+				Double x4 = ops.pop();
+				Double y4 = ops.pop();
+				ops.push(y4 / x4);
 				break;
 			default:
-				stack.push(Double.valueOf(cur));
+				ops.push(Double.valueOf(cur));
 				break;
 			}
 		}
-		return stack.pop();
+		return ops.pop();
 	}
 
-	// 把中缀表达式转换为后缀表达式
+	/**
+	 * 把中缀表达式转换为后缀表达式
+	 */
 	public static String[] InfixToPostfix(String[] input) {
-		Stack<String> stack = new Stack<String>();
-		ArrayList<String> postfix = new ArrayList<String>();
-		int index = 0;
+		Stack<String> ops = new Stack<String>();
+		ArrayList<String> vals = new ArrayList<String>();
 		for (int i = 0; i < input.length; i++) {
 			String cur = input[i].trim();
-			if (!cur.equals("+") && !cur.equals("*") && !cur.equals("(")
-					&& !cur.equals(")")) {
-				postfix.add(cur);
+			if (!cur.equals("+") && !cur.equals("*") && !cur.equals("(") && !cur.equals(")")) {
+				vals.add(cur);
 			} else if (cur.equals("+")) {
-				while (stack.isEmpty() == false) {
-					String top = stack.peek();
+				while (ops.isEmpty() == false && ops.peek().equals("(") == false) {
+					String top = ops.peek();
 					if (top.equals("*") || top.equals("+")) {
-						postfix.add(stack.pop());
-					}
-					if (top.equals("(")) {
-						break;
+						vals.add(ops.pop());
 					}
 				}
-				stack.push(cur);
+				ops.push(cur);
 			} else if (cur.equals("*")) {
-				stack.push(cur);
+				ops.push(cur);
 			} else if (cur.equals("(")) {
-				stack.push(cur);
+				ops.push(cur);
 			} else if (cur.equals(")")) {
-				while (true) {
-					String top = stack.peek();
-					if (!top.equals("(")) {
-						postfix.add(stack.pop());
-					} else {
-						stack.pop();
-						break;
-					}
+				while (ops.peek().equals("(") == false) {
+					vals.add(ops.pop());
 				}
+				ops.pop();
 			}
 		}
-		while (!stack.isEmpty()) {
-			postfix.add(stack.pop());
+		while (!ops.isEmpty()) {
+			vals.add(ops.pop());
 		}
-		String[] result = new String[postfix.size()];
-		postfix.toArray(result);
+		String[] result = new String[vals.size()];
+		vals.toArray(result);
 		return result;
 	}
 
-	//后缀表达式转为中缀表达式
+	/**
+	 * 后缀表达式转为中缀表达式,后缀表达式无括号
+	 */
 	public static String postfixToInfix(String[] input) {
-		Stack<String> stack = new Stack<String>();
+		Stack<String> vals = new Stack<String>();
 		for (int i = 0; i < input.length; i++) {
 			String cur = input[i].trim();
 			if (!cur.equals("*") && !cur.equals("+")) {
-				stack.push(cur);
+				vals.push(cur);
 			} else {
-				String right = stack.pop();
-				String left = stack.pop();
-				String tree = "("+left + cur + right+")";
-				stack.push(tree);
+				String right = vals.pop();
+				String left = vals.pop();
+				String tree = "(" + left + cur + right + ")";
+				vals.push(tree);
 			}
 		}
-		return stack.pop().trim();
+		return vals.pop().trim();
 	}
 
 	public static void main(String[] args) {
 		String test = "1 + 2 * 3 + ( 4 * 5 + 6 ) * 7";
 		String[] inputOfInfix = test.split(" ");
-		String[] inputOfPostfix = InfixPostfixP65P83
-				.InfixToPostfix(inputOfInfix);
+		String[] inputOfPostfix = InfixPostfixP65P83.InfixToPostfix(inputOfInfix);
 		System.out.println(InfixPostfixP65P83.postfix(inputOfPostfix));
 
 		String test1 = "4.99 + 5.99 + 6.99 * 1.06";
 		String[] inputOfInfix1 = test1.split(" ");
-		String[] inputOfPostfix1 = InfixPostfixP65P83
-				.InfixToPostfix(inputOfInfix1);
+		String[] inputOfPostfix1 = InfixPostfixP65P83.InfixToPostfix(inputOfInfix1);
 		System.out.println(InfixPostfixP65P83.postfix(inputOfPostfix1));
 
 		String test2 = "a b + c d e + * *";
 		String[] inputOfInfix2 = test2.split(" ");
-		String inputOfPostfix2 = InfixPostfixP65P83
-				.postfixToInfix(inputOfInfix2);
+		String inputOfPostfix2 = InfixPostfixP65P83.postfixToInfix(inputOfInfix2);
 		System.out.println(inputOfPostfix2);
 	}
 }
